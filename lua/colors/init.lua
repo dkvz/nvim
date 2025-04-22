35f2f553527450e0d9d8da443c0915a1f277bb61
dkvz <github@dkvz.eu>
2025-04-22 12:38:22 +0200

    set the correct color theme for one of my machines

--- lua/colors/init.lua
+++ lua/colors/init.lua
@@ -7,13 +7,19 @@
 --]])
 
 function SetColorScheme(color)
-	color = color or "everforest"
+	color = color or "gruvbox-material"
 	-- Setting the color scheme has to happen before
 	-- the transparency options.
-	vim.cmd.colorscheme("everforest")
+	--print("applying theme " .. color)
+	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-SetColorScheme()
+-- Look for the DKVZ_THEME env variable:
+local theme_from_env = os.getenv("DKVZ_THEME")
+if theme_from_env == "" then
+	theme_from_env = nil
+end
+SetColorScheme(theme_from_env)
