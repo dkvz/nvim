fa219d64c4ba658adbed6c03fcdd090bf691a7b0
dkvz <github@dkvz.eu>
2025-04-24 07:56:32 +0200

    correct tab options (I think)

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -6,6 +6,7 @@ vim.g.netrw_winsize = 30
 -- My old indent settings (sorry)
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
---vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.smarttab = true
