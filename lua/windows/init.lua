a0869ac7a1486151f39f16be56441ca3ea2b1302
dkvz <github@dkvz.eu>
2025-04-27 20:02:21 +0200

    remove the print statement from the windows file

--- lua/windows/init.lua
+++ lua/windows/init.lua
@@ -1,5 +1,4 @@
 -- Specific Windows options, need to manually require
 -- I should find a cleaner way to do this.
-print("required the windows file")
 vim.opt.swapfile = false
 vim.opt.ttyfast = true
