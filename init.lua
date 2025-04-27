2ebbb777a734260978cebe9a0c89b4f166375271
dkvz <github@dkvz.eu>
2025-04-27 19:37:20 +0200

    make it so I load less LSPs on Windows

--- init.lua
+++ init.lua
@@ -557,10 +557,10 @@ require("lazy").setup({
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
+				-- NOTE: These are the "minimum" LSPs, I add more in a block below
+
 				-- clangd = {},
-				gopls = {},
 				-- pyright = {},
-				rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
@@ -568,7 +568,6 @@ require("lazy").setup({
 				--
 				-- But for many setups, the LSP (`ts_ls`) will work just fine
 				ts_ls = {},
-				phpactor = {},
 				--[[emmet_ls = {
           capabilities = snip_caps,
           filetypes = {
@@ -586,7 +585,6 @@ require("lazy").setup({
 				html = {},
 				cssls = {},
 				cssmodules_ls = {},
-				bashls = {},
 				lua_ls = {
 					-- cmd = { ... },
 					-- filetypes = { ... },
@@ -602,6 +600,14 @@ require("lazy").setup({
 					},
 				},
 			}
+			if is_windows == false then
+				-- Lemme explain: I don't do much coding on my Windows nvim
+				-- What is above is my "minimum"
+				servers.gopls = {}
+				servers.rust_analyzer = {}
+				servers.phpactor = {}
+				servers.bashls = {}
+			end
 
 			-- Ensure the servers and tools above are installed
 			--
