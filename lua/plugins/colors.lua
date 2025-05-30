1f3dca18545a16ac5661b642b80c3a89915872b6
dkvz <github@dkvz.eu>
2025-05-30 13:50:41 +0200

    change the whole plugin structure

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,63 @@
+-- Legacy way to do it:
+--vim.cmd([[
+--  highlight Normal guibg=none
+--  highlight NonText guibg=none
+--  highlight Normal ctermbg=none
+--  highlight NonText ctermbg=none
+--]])
+
+function SetColorScheme(color)
+	color = color or "gruvbox-material"
+	-- Setting the color scheme has to happen before
+	-- the transparency options.
+	--print("applying theme " .. color)
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+function SetInitialColorScheme()
+	-- Look for the DKVZ_THEME env variable:
+	local theme_from_env = os.getenv("DKVZ_THEME")
+	if theme_from_env == "" then
+		theme_from_env = nil
+	end
+	SetColorScheme(theme_from_env)
+end
+
+return {
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		--'folke/tokyonight.nvim',
+		--'sainnhe/gruvbox-material',
+		"sainnhe/everforest",
+		config = function()
+			--[[require('gruvbox-material').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }--]]
+			-- Disable italic comments material:
+			vim.g.everforest_disable_italic_comment = 1
+		end,
+	},
+
+	{
+		"sainnhe/gruvbox-material",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		lazy = false,
+		config = function()
+			--[[require('gruvbox-material').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }--]]
+			-- Disable italic comments material:
+			vim.g.gruvbox_material_disable_italic_comment = 1
+		end,
+	},
+}
