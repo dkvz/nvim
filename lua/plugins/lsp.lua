c5e1456a5af123e190d102790c7131d39d33f36c
dkvz <github@dkvz.eu>
2025-06-25 09:34:06 +0200

    almost working LS in Nuxt3

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -332,22 +332,14 @@ return {
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-				automatic_installation = false,
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						-- This handles overriding only values explicitly passed
-						-- by the server configuration above. Useful when disabling
-						-- certain features of an LSP (for example, turning off formatting for TypeScript)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						--require("lspconfig")[server_name].setup(server)
-						vim.lsp.config(server_name, server)
-						vim.notify("enabled a LSP")
-					end,
-				},
-			})
+			for server_name, server in pairs(servers) do
+				server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+				-- require("lspconfig")[server_name].setup(server)
+				-- Supposed to be the way to do it now:
+				vim.lsp.config(server_name, server)
+				vim.lsp.enable(server_name)
+			end
+			-- End of the config function afterwards
 		end,
 	},
 }
